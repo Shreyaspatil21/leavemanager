f06908c0cd639aeeda3d39d74e8bfe02a1fed6a9
SHREYAS R PATIL <shreyaspatil8049@gmail.com>
2025-05-11 20:08:49 +0530

    Add core functionality for LeaveManager

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,15 @@
 <?php
-$host = 'localhost';
-$db   = 'leavemanager';
-$user = 'root';
-$pass = ''; // default WAMP MySQL password is empty
+$servername = "localhost";
+$username = "leavemanager_user"; // the username you created
+$password = "shreyas";           // the password you set for the user
+$dbname = "leavemanager";        // the name of the database
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+echo "Database connected successfully!";
 ?>
